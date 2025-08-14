d927d1e88560e3a8ecfcac70317c3650efc25c6b
Varun Kumar Shrivastava <varunshrivastava005@gmail.com>
2025-08-14 09:51:36 +0530

    iniitial setup

---
+++ src/main/java/com/lala/varun/config_server_ms_prac/ConfigServerMsPracApplication.java
@@ -0,0 +1,13 @@
+package com.lala.varun.config_server_ms_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerMsPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerMsPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lala/varun/config_server_ms_prac/ConfigServerMsPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lala.varun.config_server_ms_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerMsPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
