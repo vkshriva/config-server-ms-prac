9a9d7b2a3a209d867b072c7aad24aa5133557c7e
Varun Kumar Shrivastava <varunshrivastava005@gmail.com>
2025-08-14 11:00:04 +0530

    Added git details

--- src/main/java/com/lala/varun/config_server_ms_prac/ConfigServerMsPracApplication.java
+++ src/main/java/com/lala/varun/config_server_ms_prac/ConfigServerMsPracApplication.java
@@ -2,7 +2,9 @@ package com.lala.varun.config_server_ms_prac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+//This helps to transform a regular spring boot application into config server
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerMsPracApplication {
 
